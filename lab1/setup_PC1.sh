ff22d77c65d295e1dd42478265ac2590f0fe7e15
andrey <andrzakourcev@mail.ru>
2025-10-15 16:53:19 +0300

    new config

--- lab1/setup_PC1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-ip link add link eth1 name VLAN10 type vlan id 10
-
-ip link set VLAN10 up
-
-dhclient -v VLAN10 || exit 0
--- lab1/setup_PC2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-ip link add link eth1 name VLAN10 type vlan id 20
-
-ip link set VLAN20 up
-
-dhclient -v VLAN20 || exit 0
